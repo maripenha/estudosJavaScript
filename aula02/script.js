cb57688e08a98c9f038a06e9ba65ffef3c0047c1
Mariana Segovia Penha <158625854+maripenha@users.noreply.github.com>
2024-07-27 14:54:11 +0000

    aula 2: variaveis

---
+++ aula02/script.js
@@ -0,0 +1,16 @@
+
+
+
+// ex 1:
+// let idade = 18 
+// let nome = 'Mariana'
+
+// console.log(`Seu nome é ${nome} e você tem ${idade} anos`)
+
+
+// ex 2:
+let valor = 90
+
+let resultado = valor*5
+
+console.log(`${resultado}`)
