4ddc4c0e643ffa0a387931075de4223f1cfaeb24
MARIANA SEGOVIA PENHA <mariana.spenha@senacsp.edu.br>
2024-08-03 11:47:56 -0300

    estudos pt 03

--- aula02/script2.js
+++ aula02/script2.js
@@ -8,10 +8,10 @@
 // console.log(`Seu nome é ${nome} e você tem ${idade} anos`)
 
 
-let num1 = parseFloat(prompt('Insira a primeira nota:'))
-let num2 = parseFloat(prompt('Insira a segunda nota:'))
-let num3 = parseFloat(prompt('Insira a terceira nota:'))
+// let num1 = parseFloat(prompt('Insira a primeira nota:'))
+// let num2 = parseFloat(prompt('Insira a segunda nota:'))
+// let num3 = parseFloat(prompt('Insira a terceira nota:'))
 
-let mediaFinal = (num1+num2+num3) / 3;
+// let mediaFinal = (num1+num2+num3) / 3;
 
-alert(`Sua média é:  ${mediaFinal}`)
+// alert(`Sua média é:  ${mediaFinal}`)
