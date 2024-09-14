13b1d8dcd4cda4488270a3ab889f81392edba7c8
Mariana Segovia Penha <158625854+maripenha@users.noreply.github.com>
2024-09-14 14:33:35 +0000

    aula 07

---
+++ aula07/script7.js
@@ -0,0 +1,69 @@
+const titulo = document.querySelector('#titulo');
+const resposta = document.querySelector('#respostaAtributo')
+
+function pegarClasse(){
+    resposta.textContent = titulo.getAttribute('class')
+}
+
+const buttonClass = document.querySelector('#pegarClasse');
+buttonClass.addEventListener('click', pegarClasse);
+
+function verificarClasse(){
+    resposta.textContent = titulo.hasAttribute('class')
+
+}
+
+const buttonVerificar = document.querySelector('#verificarClasse');
+buttonVerificar.addEventListener('click', verificarClasse);
+
+function trocar(){
+    titulo.setAttribute('class', 'tituloFundoRosa')
+}
+
+const buttonTrocar = document.querySelector('#trocarClasse');
+buttonTrocar.addEventListener('click', trocar)
+
+function variar(){
+    titulo.classList.remove(titulo.getAttribute('class', '' ))
+}
+
+const buttonVariar = document.querySelector('#variarClasse');
+buttonVariar.addEventListener('click', variar)
+
+
+
+
+// const buttonLampada = document.querySelector('#variar');
+// function buttonChange(){
+//     buttonLampada.innerHTML = "Apagar"
+// }
+
+
+// const lampada = document.querySelector('#lampada');
+// const body = document.querySelector('body')z
+
+// function ascenderLampada(){
+    
+//     lampada.setAttribute('src', 'assetsaula07/acesa.png')
+    
+// }
+
+
+// buttonLampada.addEventListener('click', buttonChange, ascenderLampada)
+
+
+// function ascenderLampada(){
+//     const lampada = document.querySelector('#lampada');
+//     const body = document.querySelector('body')
+
+//     if (){
+//         body.classList.toggle('acesa')
+//     }
+    
+// }
+
+
+
+// jokenpo
+
+
